d74e8188bb048d2b768b68c33d4bc35e323c094a
estur28 <89254564126@mail.ru>
2021-01-30 19:11:07 +0300

    feat: added Title component

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Title.scss'
+
+const Title = () => (
+        <div className='center'>
+            <h1 className="title">BreakingBadApp</h1>
+            <p className="subtitle">Are you looking for a favorite character?</p>
+        </div>
+    )
+
+export default Title

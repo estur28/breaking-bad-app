ec4c0e7a5977f000e29b6e69554cfd1c2752a0d7
estur28 <89254564126@mail.ru>
2021-01-30 19:06:20 +0300

    feat: added App's logic and  axios requests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from '../node_modules/axios';
+import AllCharacters from './pages/characters/AllCharacters'
+import './App.scss';
+import Title from './components/Title'
+import Search from './components/Search'
 
-function App() {
+
+const App = () => {
+  
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+ 
+ 
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      )
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
+
+    
   return (
-    <div className="App">
-     hi
+    <div className='container'>
+      <Title />
+      <Search getQuery={(q) => setQuery(q)} />
+      <AllCharacters isLoading={isLoading} items={items} />
     </div>
-  );
+  )
 }
 
+
+
 export default App;
+
